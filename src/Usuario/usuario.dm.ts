7425b0aa9e4d14eb79c31572f50d9a3551f949cf
Michele <michele.tkkataoka@senacsp.edu.br>
2025-02-25 20:08:53 -0300

    validação de usuario

---
+++ src/Usuario/usuario.dm.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@nestjs/common";
+import { UsuarioEntity } from "./usuario.entity";
+
+@Injectable()
+export class UsuariosArmazenados{
+    #usuarios: UsuarioEntity[] = [];
+
+    AdicionarUsuario(usuario: UsuarioEntity){
+        this.#usuarios.push(usuario);
+    }
+
+    get Usuarios(){
+        return this.#usuarios;
+    }
+
+    validarUsuario(dadosUsuario){
+        var validacoes: string[]=[];
+        if(!(dadosUsuario.id != null)){
+            validacoes.push("Id não pode ser nulo")
+        }
+        
+    
+        if(!(dadosUsuario.nome != null)){
+            validacoes.push("Nome não pode ser nulo")
+        }
+        if(!(dadosUsuario.idade != null)){ 
+            validacoes.push("Idade não pode ser nulo")
+        }
+        if(!(dadosUsuario.cidade != null)){
+            validacoes.push("Cidade não pode ser nulo")
+        }
+
+        if(!(dadosUsuario.email != null)){
+            validacoes.push("Email não pode ser nulo")
+        }
+        if(!(dadosUsuario.telefone != null)){
+            validacoes.push("Telefone não pode ser nulo")
+        }
+        if(!(dadosUsuario.senha != null)){
+            validacoes.push("Senha não pode ser nulo")
+        }
+
+        return validacoes;
+
+
+    }
+
+
+}
